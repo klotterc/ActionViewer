9400509fcf00db6624c71b3dead10a4fd5d46149
ClassicRagu <megameowthethird@gmail.com>
2024-03-30 13:42:24 -0500

    Refactor tables and code cleanup

--- ActionSnoop/Functions/StatusInfoFunctions.cs
+++ ActionSnoop/Functions/StatusInfoFunctions.cs
@@ -1,8 +1,12 @@
-﻿using ActionSnoop.Models;
+﻿using ActionSnoop.Enums;
+using ActionSnoop.Models;
+using Dalamud.Game.ClientState.Objects.SubKinds;
 using Dalamud.Game.ClientState.Statuses;
+using ImGuiNET;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +15,7 @@ namespace ActionSnoop.Functions
     public static class StatusInfoFunctions
     {
         private static List<int> essenceIds = new List<int>() { 2311, 2312, 2313, 2314, 2315, 2316, 2317, 2318, 2319, 2320, 2321, 2322, 2323, 2324, 2325, 2434, 2435, 2436, 2437, 2438, 2439, };
-        public static StatusInfo GetStatusInfo(StatusList statusList)
+        private static StatusInfo GetStatusInfo(StatusList statusList)
         {
             int leftId = 0;
             StatusInfo statusInfo = new StatusInfo();
@@ -39,6 +43,67 @@ namespace ActionSnoop.Functions
                 }
             }
             return statusInfo;
+        }
+
+        public static void GenerateStatusTable(List<PlayerCharacter> playerCharacters, string searchText)
+        {
+            ImGuiTableFlags tableFlags = ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg;// | ImGuiTableFlags.SizingFixedFit;
+            var iconSize = ImGui.GetTextLineHeight() * 2f;
+            var iconSizeVec = new Vector2(iconSize, iconSize);
+
+            string playerName;
+            uint jobId;
+            StatusList statusList;
+
+            if (ImGui.BeginTable("table1", 5, tableFlags))
+            {
+                ImGui.TableSetupColumn("Job", ImGuiTableColumnFlags.WidthFixed, 34f);
+                ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.WidthStretch);
+                ImGui.TableSetupColumn("Ess.", ImGuiTableColumnFlags.WidthFixed, 34f);
+                ImGui.TableSetupColumn("Left", ImGuiTableColumnFlags.WidthFixed, 34f);
+                ImGui.TableSetupColumn("Right", ImGuiTableColumnFlags.WidthFixed, 34f);
+                ImGui.TableHeadersRow();
+                foreach (PlayerCharacter character in playerCharacters)
+                {
+                    // get player name, job ID, status list
+                    playerName = character.Name.ToString();
+                    String[] playerFirstLast = playerName.Split(" ");
+                    jobId = (uint)character.ClassJob.GameData?.JobIndex;
+                    statusList = character.StatusList;
+
+                    /* use status list to find:
+                     * char essence ID (or -1)
+                     * char reminiscence ID (or -1)
+                     */
+
+                    StatusInfo statusInfo = StatusInfoFunctions.GetStatusInfo(statusList);
+
+                    if (searchText == string.Empty ||
+                        (statusInfo.rightIconID != 33 && ((BozjaActions)statusInfo.rightId).ToString().Replace("_", " ").ToLowerInvariant().IndexOf(searchText.ToLowerInvariant()) != -1) ||
+                        (statusInfo.leftIconID != 33 && ((BozjaActions)statusInfo.leftId).ToString().Replace("_", " ").ToLowerInvariant().IndexOf(searchText.ToLowerInvariant()) != -1))
+                    {
+
+                        // player job, name
+                        ImGui.TableNextColumn();
+                        //ImGui.Image(Plugin.TextureProvider.GetIcon(62400 + jobId)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
+                        ImGui.Image(Plugin.TextureProvider.GetIcon(62400 + jobId)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
+                        ImGui.TableNextColumn();
+                        ImGui.Text(playerFirstLast[0] + "\n" + playerFirstLast[1]);
+
+                        // essence
+                        ImGui.TableNextColumn();
+                        ImGui.Image(Plugin.TextureProvider.GetIcon(statusInfo.essenceIconID)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
+
+                        // left/right actions
+                        ImGui.TableNextColumn();
+                        ImGui.Image(Plugin.TextureProvider.GetIcon(statusInfo.leftIconID)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
+                        ImGui.TableNextColumn();
+                        ImGui.Image(Plugin.TextureProvider.GetIcon(statusInfo.rightIconID)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
+                    }
+                }
+                ImGui.EndTable();
+
+            }
         }
     }
 }
--- ActionSnoop/Tabs/HealerTab.cs
+++ ActionSnoop/Tabs/HealerTab.cs
@@ -25,70 +25,10 @@ public class HealerTab : MainWindowTab
 
     public override void Draw()
     {
-        //ImGui.BeginChild("##action_snoop_internal", ImGuiHelpers.ScaledVector2(-1f, -25f));
-        //ImGui.SetNextWindowSizeConstraints(new Vector2(UIMinWidth, UIMinHeight), new Vector2(UIMaxWidth, UIMaxHeight));
-        //List<PlayerCharacter> playerCharacters = actionSnoop.getDummyList();
         List<PlayerCharacter> playerCharacters = this.Plugin.ActionSnoop.getPlayerCharacters().Where(x => healers.Contains((uint)x.ClassJob.GameData?.JobIndex)).ToList();
-
-        ImGuiTableFlags tableFlags = ImGuiTableFlags.Borders | ImGuiTableFlags.RowBg;// | ImGuiTableFlags.SizingFixedFit;
-        var iconSize = ImGui.GetTextLineHeight() * 2f;
-        var iconSizeVec = new Vector2(iconSize, iconSize);
-
-        string playerName;
-        uint jobId;
-        StatusList statusList;
 
         ImGui.SetNextItemWidth(-1 * ImGui.GetIO().FontGlobalScale);
         ImGui.InputText("", ref searchText, 256);
-        if (ImGui.BeginTable("table1", 5, tableFlags))
-        {
-            ImGui.TableSetupColumn("Job", ImGuiTableColumnFlags.WidthFixed, 34f);
-            ImGui.TableSetupColumn("Name", ImGuiTableColumnFlags.WidthStretch);
-            ImGui.TableSetupColumn("Ess.", ImGuiTableColumnFlags.WidthFixed, 34f);
-            ImGui.TableSetupColumn("Left", ImGuiTableColumnFlags.WidthFixed, 34f);
-            ImGui.TableSetupColumn("Right", ImGuiTableColumnFlags.WidthFixed, 34f);
-            ImGui.TableHeadersRow();
-            foreach (PlayerCharacter character in playerCharacters)
-            {
-                // get player name, job ID, status list
-                playerName = character.Name.ToString();
-                String[] playerFirstLast = playerName.Split(" ");
-                jobId = (uint)character.ClassJob.GameData?.JobIndex;
-                statusList = character.StatusList;
-
-                /* use status list to find:
-                 * char essence ID (or -1)
-                 * char reminiscence ID (or -1)
-                 */
-
-                StatusInfo statusInfo = StatusInfoFunctions.GetStatusInfo(statusList);
-
-                if (searchText == string.Empty ||
-                    (statusInfo.rightIconID != 33 && ((BozjaActions)statusInfo.rightId).ToString().Replace("_", " ").ToLowerInvariant().IndexOf(searchText.ToLowerInvariant()) != -1) ||
-                    (statusInfo.leftIconID != 33 && ((BozjaActions)statusInfo.leftId).ToString().Replace("_", " ").ToLowerInvariant().IndexOf(searchText.ToLowerInvariant()) != -1))
-                {
-
-                    // player job, name
-                    ImGui.TableNextColumn();
-                    //ImGui.Image(Plugin.TextureProvider.GetIcon(62400 + jobId)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
-                    ImGui.Image(Plugin.TextureProvider.GetIcon(62400 + jobId)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
-                    ImGui.TableNextColumn();
-                    ImGui.Text(playerFirstLast[0] + "\n" + playerFirstLast[1]);
-
-                    // essence
-                    ImGui.TableNextColumn();
-                    ImGui.Image(Plugin.TextureProvider.GetIcon(statusInfo.essenceIconID)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
-
-                    // left/right actions
-                    ImGui.TableNextColumn();
-                    ImGui.Image(Plugin.TextureProvider.GetIcon(statusInfo.leftIconID)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
-                    ImGui.TableNextColumn();
-                    ImGui.Image(Plugin.TextureProvider.GetIcon(statusInfo.rightIconID)!.ImGuiHandle, iconSizeVec, Vector2.Zero, Vector2.One);
-                }
-            }
-            ImGui.EndTable();
-
-        }
-        //ImGui.EndChild();
+        StatusInfoFunctions.GenerateStatusTable(playerCharacters, searchText);
     }
 }
